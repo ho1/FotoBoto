5904a66e62766622744a290ebc767167f58b7c1e
Henning Olsson <henningo@gmail.com>
2015-06-21 11:23:57 -0600

    Visual Studio Project commit
    Initial commit of the visual studio project with a barebones GUI. There
    could probably be some cleaning of the OpenCV dlls, but it is kept like
    this for ease of use.

---
+++ FotoBoto/MainForm.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.RefreshCamTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.FourPicTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.CameraStatusLabel = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ProcessLabel = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'RefreshCamTimer
+        '
+        '
+        'FourPicTimer
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'PrintDocument1
+        '
+        '
+        'CameraStatusLabel
+        '
+        Me.CameraStatusLabel.AutoSize = True
+        Me.CameraStatusLabel.Location = New System.Drawing.Point(144, 31)
+        Me.CameraStatusLabel.Name = "CameraStatusLabel"
+        Me.CameraStatusLabel.Size = New System.Drawing.Size(107, 13)
+        Me.CameraStatusLabel.TabIndex = 0
+        Me.CameraStatusLabel.Text = "Camera is connected"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 103)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(198, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Press right mouse button to start process"
+        '
+        'ProcessLabel
+        '
+        Me.ProcessLabel.AutoSize = True
+        Me.ProcessLabel.Location = New System.Drawing.Point(144, 173)
+        Me.ProcessLabel.Name = "ProcessLabel"
+        Me.ProcessLabel.Size = New System.Drawing.Size(119, 13)
+        Me.ProcessLabel.TabIndex = 2
+        Me.ProcessLabel.Text = "Process is NOT running"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(140, 387)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(162, 13)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "www.github.com/ho1/FotoBoto/"
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(419, 409)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.ProcessLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CameraStatusLabel)
+        Me.Name = "MainForm"
+        Me.Text = "FotoBoto - Photobooth software"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ImageBox1 As Emgu.CV.UI.ImageBox
+    Friend WithEvents RefreshCamTimer As System.Windows.Forms.Timer
+    Friend WithEvents FourPicTimer As System.Windows.Forms.Timer
+    Friend WithEvents PrintPreviewDialog1 As System.Windows.Forms.PrintPreviewDialog
+    Friend WithEvents PrintDocument1 As System.Drawing.Printing.PrintDocument
+    Friend WithEvents CameraStatusLabel As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ProcessLabel As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ FotoBoto/MainForm.vb
@@ -0,0 +1,230 @@
+﻿''' <summary>
+''' The MIT License (MIT)
+'''
+'''Copyright(c) 2015 Henning Olsson
+'''
+'''Permission Is hereby granted, free Of charge, to any person obtaining a copy
+'''of this software And associated documentation files (the "Software"), to deal
+'''in the Software without restriction, including without limitation the rights
+'''to use, copy, modify, merge, publish, distribute, sublicense, And/Or sell
+'''copies of the Software, And to permit persons to whom the Software Is
+'''furnished to do so, subject to the following conditions:
+'''
+'''The above copyright notice And this permission notice shall be included In all
+'''copies Or substantial portions of the Software.
+'''
+'''THE SOFTWARE Is PROVIDED "AS IS", WITHOUT WARRANTY Of ANY KIND, EXPRESS Or
+'''IMPLIED, INCLUDING BUT Not LIMITED To THE WARRANTIES Of MERCHANTABILITY,
+'''FITNESS For A PARTICULAR PURPOSE And NONINFRINGEMENT. In NO Event SHALL THE
+'''AUTHORS Or COPYRIGHT HOLDERS BE LIABLE For ANY CLAIM, DAMAGES Or OTHER
+'''LIABILITY, WHETHER In AN ACTION Of CONTRACT, TORT Or OTHERWISE, ARISING FROM,
+'''OUT Of Or In CONNECTION With THE SOFTWARE Or THE USE Or OTHER DEALINGS In THE
+'''SOFTWARE.
+'''
+''' </summary>
+
+
+Imports Emgu.CV
+Imports Emgu.CV.UI
+Imports Emgu.CV.Structure
+Imports Emgu.CV.CvEnum
+
+Public Class MainForm
+
+#Region "Init"
+
+    Public Camera As Capture
+    Public Frame As Emgu.CV.Image(Of Bgr, Byte)
+    Public SaveFrame1 As Emgu.CV.Image(Of Bgr, Byte)
+    Public SaveFrame2 As Emgu.CV.Image(Of Bgr, Byte)
+    Public SaveFrame3 As Emgu.CV.Image(Of Bgr, Byte)
+    Public SaveFrame4 As Emgu.CV.Image(Of Bgr, Byte)
+    Public FileNameTick As String ''This is for holding the main photofile names for each run
+    Public FourPicElapsed As Double = 0 ''This will containt number of ms since we started the timer which controls to process
+
+
+    Private Sub MainForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        SetupCamera()
+        RefreshCamTimer.Interval = 50
+        RefreshCamTimer.Start()
+
+    End Sub
+
+    Public Sub SetupCamera()
+        Camera = New Capture
+        Camera.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 1920)
+        Camera.SetCaptureProperty(CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 1080)
+        Me.CameraStatusLabel.Text = "Camera is CONNECTED"
+
+    End Sub
+
+#End Region
+
+#Region "Take 4 Pics Process"
+
+    Public Sub LaunchPhotoProcess()
+
+        Dim tempfileName As String = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") ''The file name will basically be a datestamp
+        FileNameTick = tempfileName.Replace(":", "")
+
+        FourPicElapsed = 0
+        FourPicTimer.Start()
+
+    End Sub
+
+    Public Sub FourTriggerProcess(ByVal Time As Double)
+
+        Select Case Time
+            Case 1000
+                SaveFrame(FileNameTick & "_1")
+            Case 2000
+                SaveFrame(FileNameTick & "_2")
+            Case 3000
+                SaveFrame(FileNameTick & "_3")
+            Case 4000
+                SaveFrame(FileNameTick & "_4")
+
+            Case 5000
+
+                FourPicTimer.Stop()
+                MergePictures()
+                FourPicElapsed = 0
+                Me.ProcessLabel.Text = "Process is NOT running"
+        End Select
+
+    End Sub
+
+    Private Sub FourPicTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FourPicTimer.Tick
+
+        FourPicElapsed += FourPicTimer.Interval
+        FourTriggerProcess(FourPicElapsed)
+
+    End Sub
+
+
+    Private Sub RefreshCamTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RefreshCamTimer.Tick
+        RefreshFrame()
+    End Sub
+
+#End Region
+
+#Region "Image Helper Functions"
+
+    Public Sub SaveFrame(ByVal aFilename As String)
+
+        ''Grab the most recent frame and save it
+        SaveFrame1 = ProcessFrame()
+        SaveFrame1.Save(aFilename & ".jpg")
+        My.Computer.Audio.Play("snap1.wav") ''Confirms for user that photo has been taken and they can move to next pose! Source: https://www.freesound.org/people/thecheeseman/sounds/51360/
+
+    End Sub
+
+    Public Overloads Shared Function CropBitmap(ByVal srcBitmap As Bitmap, ByVal cropX As Integer, ByVal cropY As Integer, ByVal cropWidth As Integer, ByVal cropHeight As Integer) As Bitmap
+
+        ' Create the new bitmap and associated graphics object
+        Dim bmp As New Bitmap(cropWidth, cropHeight)
+
+        Dim g As Graphics = Graphics.FromImage(bmp)
+        ' Draw the specified section of the source bitmap to the new one
+        g.DrawImage(srcBitmap, New Rectangle(0, 0, cropWidth, cropHeight), cropX, cropY, cropWidth, cropHeight, GraphicsUnit.Pixel)
+        ' Clean up
+        g.Dispose()
+
+        ' Return the bitmap
+        Return bmp
+
+    End Function
+
+    Public Sub MergePictures()
+
+        ''Assemble all four shots and add graphic to create a photostrip
+        Dim imageFile As New Bitmap(1600, 4800)
+
+        Dim maingraphic As Graphics = Graphics.FromImage(imageFile)
+        maingraphic.Clear(Color.White)
+
+        maingraphic.DrawImage(CropBitmap(Image.FromFile(FileNameTick & "_1.jpg"), 240, 0, 1440, 1080), New Point(80, 80))
+        maingraphic.DrawImage(CropBitmap(Image.FromFile(FileNameTick & "_2.jpg"), 240, 0, 1440, 1080), New Point(80, 80 + 1080))
+        maingraphic.DrawImage(CropBitmap(Image.FromFile(FileNameTick & "_3.jpg"), 240, 0, 1440, 1080), New Point(80, 80 + 1080 * 2))
+        maingraphic.DrawImage(CropBitmap(Image.FromFile(FileNameTick & "_4.jpg"), 240, 0, 1440, 1080), New Point(80, 80 + 1080 * 3))
+        maingraphic.DrawImage(Image.FromFile("logo.jpg"), New Point(0, 4400))
+
+        imageFile.Save(FileNameTick & "_single.jpg", Imaging.ImageFormat.Jpeg)
+
+        '' Assemble the two strips for print on 4x3 photo paper
+        Dim bm As New Bitmap(3200, 4800)
+        Dim gr As Graphics = Graphics.FromImage(bm)
+        gr.DrawImage(Image.FromFile(FileNameTick & "_single.jpg"), New Point(0, 0))
+        gr.DrawImage(Image.FromFile(FileNameTick & "_single.jpg"), New Point(1600, 0))
+        bm.Save(FileNameTick & "_double.jpg", Imaging.ImageFormat.Jpeg)
+
+        gr.Dispose()
+        bm.Dispose()
+        imageFile.Dispose()
+        maingraphic.Dispose()
+
+        ''Launch the printing
+        PrintDocument1.Print()
+
+        ''This may not be necssary, but seems to keep the memory footprint lower (disposing all the unused bitmaps)
+        GC.Collect()
+    End Sub
+#End Region
+
+#Region "Camera Updates"
+    Public Sub RefreshFrame()
+
+        ''Grab the most recent frame using Emgy.CV
+        Frame = Camera.QueryFrame()
+
+    End Sub
+
+    Public Function ProcessFrame() As Emgu.CV.Image(Of Bgr, Byte)
+
+        ''Return a copy of the most recent frame for further processing/saving
+        Return Frame.Copy()
+
+    End Function
+#End Region
+
+#Region "Print Stuff"
+
+    Private Sub PD_PrintPage(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+
+        ''This prints the assembled photo to the default printer. This may require some tinkering depending on your system.
+        Dim mainPic As Image = Image.FromFile(FileNameTick & "_double.jpg")
+        e.Graphics.DrawImage(mainPic, e.Graphics.VisibleClipBounds)
+        e.Graphics.DrawImage(mainPic, e.Graphics.VisibleClipBounds)
+        e.HasMorePages = False
+
+    End Sub
+
+#End Region
+
+#Region "USB Button Click"
+
+    Private Sub Button1_MouseDown(ByVal ByValsender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown
+        ''Note that the MainForm window has to be in focus to get this event to trigger
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+
+            If FourPicElapsed = 0 Then
+                Me.ProcessLabel.Text = "Process is RUNNING"
+
+                My.Computer.Audio.Play("powerup.wav") ''Confirms for user that button has been pressed and process started. Source: http://themushroomkingdom.net/media/smb/wav
+                LaunchPhotoProcess()
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://www.github.com/ho1/FotoBoto/")
+    End Sub
+
+#End Region
+
+
+
+
+End Class
